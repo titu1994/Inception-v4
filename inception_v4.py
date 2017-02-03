af62d6f4e7f133ee0e876a52efbd23b603880ee6
Somshubra Majumdar <titu1994@gmail.com>
2017-02-03 00:35:14 -0600

    Updated inception v4 model (thanks to @kentsommer)

--- inception_v4.py
+++ inception_v4.py
@@ -9,39 +9,51 @@ from keras import backend as K
 Implementation of Inception Network v4 [Inception Network v4 Paper](http://arxiv.org/pdf/1602.07261v1.pdf) in Keras.
 """
 
-def inception_stem(input): # Input (299,299,3)
+TF_BACKEND_TF_DIM_ORDERING = "weights/inception_v4_tf_backend_tf_dim_ordering.h5"
+
+def conv_block(x, nb_filter, nb_row, nb_col, border_mode='same', subsample=(1, 1), bias=False):
+    if K.image_dim_ordering() == "th":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    x = Convolution2D(nb_filter, nb_row, nb_col, subsample=subsample, border_mode=border_mode, bias=bias)(x)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = Activation('relu')(x)
+    return x
+
+
+def inception_stem(input):
     if K.image_dim_ordering() == "th":
         channel_axis = 1
     else:
         channel_axis = -1
 
     # Input Shape is 299 x 299 x 3 (th) or 3 x 299 x 299 (th)
-    c = Convolution2D(32, 3, 3, activation='relu', subsample=(2,2), bias=False)(input)
-    c = Convolution2D(32, 3, 3, activation='relu', bias=False)(c)
-    c = Convolution2D(64, 3, 3, activation='relu', border_mode='same', bias=False)(c)
+    x = conv_block(input, 32, 3, 3, subsample=(2, 2), border_mode='valid')
+    x = conv_block(x, 32, 3, 3, border_mode='valid')
+    x = conv_block(x, 64, 3, 3)
 
-    c1 = MaxPooling2D((3,3), strides=(2,2))(c)
-    c2 = Convolution2D(96, 3, 3, activation='relu', subsample=(2,2), bias=False)(c)
+    x1 = MaxPooling2D((3, 3), strides=(2, 2), border_mode='valid')(x)
+    x2 = conv_block(x, 96, 3, 3, subsample=(2, 2), border_mode='valid')
 
-    m = merge([c1, c2], mode='concat', concat_axis=channel_axis)
+    x = merge([x1, x2], mode='concat', concat_axis=channel_axis)
 
-    c1 = Convolution2D(64, 1, 1, activation='relu', border_mode='same', bias=False)(m)
-    c1 = Convolution2D(96, 3, 3, activation='relu', bias=False)(c1)
+    x1 = conv_block(x, 64, 1, 1)
+    x1 = conv_block(x1, 96, 3, 3, border_mode='valid')
 
-    c2 = Convolution2D(64, 1, 1, activation='relu', border_mode='same', bias=False)(m)
-    c2 = Convolution2D(64, 7, 1, activation='relu', border_mode='same', bias=False)(c2)
-    c2 = Convolution2D(64, 1, 7, activation='relu', border_mode='same', bias=False)(c2)
-    c2 = Convolution2D(96, 3, 3, activation='relu', border_mode='valid', bias=False)(c2)
+    x2 = conv_block(x, 64, 1, 1)
+    x2 = conv_block(x2, 64, 1, 7)
+    x2 = conv_block(x2, 64, 7, 1)
+    x2 = conv_block(x2, 96, 3, 3, border_mode='valid')
 
-    m2 = merge([c1, c2], mode='concat', concat_axis=channel_axis)
+    x = merge([x1, x2], mode='concat', concat_axis=channel_axis)
 
-    p1 = MaxPooling2D((3,3), strides=(2,2), )(m2)
-    p2 = Convolution2D(192, 3, 3, activation='relu', subsample=(2,2), bias=False)(m2)
+    x1 = conv_block(x, 192, 3, 3, subsample=(2, 2), border_mode='valid')
+    x2 = MaxPooling2D((3, 3), strides=(2, 2), border_mode='valid')(x)
 
-    m3 = merge([p1, p2], mode='concat', concat_axis=channel_axis)
-    m3 = BatchNormalization(axis=channel_axis)(m3)
-    m3 = Activation('relu')(m3)
-    return m3
+    x = merge([x1, x2], mode='concat', concat_axis=channel_axis)
+    return x
 
 
 def inception_A(input):
@@ -50,22 +62,21 @@ def inception_A(input):
     else:
         channel_axis = -1
 
-    a1 = AveragePooling2D((3,3), strides=(1,1), border_mode='same')(input)
-    a1 = Convolution2D(96, 1, 1, activation='relu', border_mode='same', bias=False)(a1)
+    a1 = conv_block(input, 96, 1, 1)
 
-    a2 = Convolution2D(96, 1, 1, activation='relu', border_mode='same', bias=False)(input)
+    a2 = conv_block(input, 64, 1, 1)
+    a2 = conv_block(a2, 96, 3, 3)
 
-    a3 = Convolution2D(64, 1, 1, activation='relu', border_mode='same', bias=False)(input)
-    a3 = Convolution2D(96, 3, 3, activation='relu', border_mode='same', bias=False)(a3)
+    a3 = conv_block(input, 64, 1, 1)
+    a3 = conv_block(a3, 96, 3, 3)
+    a3 = conv_block(a3, 96, 3, 3)
 
-    a4 = Convolution2D(64, 1, 1, activation='relu', border_mode='same', bias=False)(input)
-    a4 = Convolution2D(96, 3, 3, activation='relu', border_mode='same', bias=False)(a4)
-    a4 = Convolution2D(96, 3, 3, activation='relu', border_mode='same', bias=False)(a4)
+    a4 = AveragePooling2D((3, 3), strides=(1, 1), border_mode='same')(input)
+    a4 = conv_block(a4, 96, 1, 1)
 
     m = merge([a1, a2, a3, a4], mode='concat', concat_axis=channel_axis)
-    m = BatchNormalization(axis=channel_axis)(m)
-    m = Activation('relu')(m)
     return m
+
 
 def inception_B(input):
     if K.image_dim_ordering() == "th":
@@ -73,25 +84,24 @@ def inception_B(input):
     else:
         channel_axis = -1
 
-    b1 = AveragePooling2D((3,3), strides=(1,1), border_mode='same')(input)
-    b1 = Convolution2D(128, 1, 1, activation='relu', border_mode='same', bias=False)(b1)
+    b1 = conv_block(input, 384, 1, 1)
 
-    b2 = Convolution2D(384, 1, 1, activation='relu', border_mode='same', bias=False)(input)
+    b2 = conv_block(input, 192, 1, 1)
+    b2 = conv_block(b2, 224, 1, 7)
+    b2 = conv_block(b2, 256, 7, 1)
 
-    b3 = Convolution2D(192, 1, 1, activation='relu', border_mode='same', bias=False)(input)
-    b3 = Convolution2D(224, 1, 7, activation='relu', border_mode='same', bias=False)(b3)
-    b3 = Convolution2D(256, 1, 7, activation='relu', border_mode='same', bias=False)(b3)
+    b3 = conv_block(input, 192, 1, 1)
+    b3 = conv_block(b3, 192, 7, 1)
+    b3 = conv_block(b3, 224, 1, 7)
+    b3 = conv_block(b3, 224, 7, 1)
+    b3 = conv_block(b3, 256, 1, 7)
 
-    b4 = Convolution2D(192, 1, 1, activation='relu', border_mode='same', bias=False)(input)
-    b4 = Convolution2D(192, 1, 7, activation='relu', border_mode='same', bias=False)(b4)
-    b4 = Convolution2D(224, 7, 1, activation='relu', border_mode='same', bias=False)(b4)
-    b4 = Convolution2D(224, 1, 7, activation='relu', border_mode='same', bias=False)(b4)
-    b4 = Convolution2D(256, 7, 1, activation='relu', border_mode='same', bias=False)(b4)
+    b4 = AveragePooling2D((3, 3), strides=(1, 1), border_mode='same')(input)
+    b4 = conv_block(b4, 128, 1, 1)
 
     m = merge([b1, b2, b3, b4], mode='concat', concat_axis=channel_axis)
-    m = BatchNormalization(axis=channel_axis)(m)
-    m = Activation('relu')(m)
     return m
+
 
 def inception_C(input):
     if K.image_dim_ordering() == "th":
@@ -99,44 +109,44 @@ def inception_C(input):
     else:
         channel_axis = -1
 
-    c1 = AveragePooling2D((3, 3), strides=(1, 1), border_mode='same')(input)
-    c1 = Convolution2D(256, 1, 1, activation='relu', border_mode='same', bias=False)(c1)
+    c1 = conv_block(input, 256, 1, 1)
 
-    c2 = Convolution2D(256, 1, 1, activation='relu', border_mode='same', bias=False)(input)
+    c2 = conv_block(input, 384, 1, 1)
+    c2_1 = conv_block(c2, 256, 1, 3)
+    c2_2 = conv_block(c2, 256, 3, 1)
+    c2 = merge([c2_1, c2_2], mode='concat', concat_axis=channel_axis)
 
-    c3 = Convolution2D(384, 1, 1, activation='relu', border_mode='same', bias=False)(input)
-    c3_1 = Convolution2D(256, 1, 3, activation='relu', border_mode='same', bias=False)(c3)
-    c3_2 = Convolution2D(256, 3, 1, activation='relu', border_mode='same', bias=False)(c3)
+    c3 = conv_block(input, 384, 1, 1)
+    c3 = conv_block(c3, 448, 3, 1)
+    c3 = conv_block(c3, 512, 1, 3)
+    c3_1 = conv_block(c3, 256, 1, 3)
+    c3_2 = conv_block(c3, 256, 3, 1)
+    c3 = merge([c3_1, c3_2], mode='concat', concat_axis=channel_axis)
 
-    c4 = Convolution2D(384, 1, 1, activation='relu', border_mode='same', bias=False)(input)
-    c4 = Convolution2D(192, 1, 3, activation='relu', border_mode='same', bias=False)(c4)
-    c4 = Convolution2D(224, 3, 1, activation='relu', border_mode='same', bias=False)(c4)
-    c4_1 = Convolution2D(256, 3, 1, activation='relu', border_mode='same', bias=False)(c4)
-    c4_2 = Convolution2D(256, 1, 3, activation='relu', border_mode='same', bias=False)(c4)
+    c4 = AveragePooling2D((3, 3), strides=(1, 1), border_mode='same')(input)
+    c4 = conv_block(c4, 256, 1, 1)
 
-    m = merge([c1, c2, c3_1, c3_2, c4_1, c4_2], mode='concat', concat_axis=channel_axis)
-    m = BatchNormalization(axis=channel_axis)(m)
-    m = Activation('relu')(m)
+    m = merge([c1, c2, c3, c4], mode='concat', concat_axis=channel_axis)
     return m
 
-def reduction_A(input, k=192, l=224, m=256, n=384):
+
+def reduction_A(input):
     if K.image_dim_ordering() == "th":
         channel_axis = 1
     else:
         channel_axis = -1
 
-    r1 = MaxPooling2D((3,3), strides=(2,2))(input)
+    r1 = conv_block(input, 384, 3, 3, subsample=(2, 2), border_mode='valid')
 
-    r2 = Convolution2D(n, 3, 3, activation='relu', subsample=(2,2), bias=False)(input)
+    r2 = conv_block(input, 192, 1, 1)
+    r2 = conv_block(r2, 224, 3, 3)
+    r2 = conv_block(r2, 256, 3, 3, subsample=(2, 2), border_mode='valid')
 
-    r3 = Convolution2D(k, 1, 1, activation='relu', border_mode='same', bias=False)(input)
-    r3 = Convolution2D(l, 3, 3, activation='relu', border_mode='same', bias=False)(r3)
-    r3 = Convolution2D(m, 3, 3, activation='relu', subsample=(2,2), bias=False)(r3)
+    r3 = MaxPooling2D((3, 3), strides=(2, 2), border_mode='valid')(input)
 
     m = merge([r1, r2, r3], mode='concat', concat_axis=channel_axis)
-    m = BatchNormalization(axis=channel_axis)(m)
-    m = Activation('relu')(m)
     return m
+
 
 def reduction_B(input):
     if K.image_dim_ordering() == "th":
@@ -144,23 +154,21 @@ def reduction_B(input):
     else:
         channel_axis = -1
 
-    r1 = MaxPooling2D((3, 3), strides=(2, 2))(input)
+    r1 = conv_block(input, 192, 1, 1)
+    r1 = conv_block(r1, 192, 3, 3, subsample=(2, 2), border_mode='valid')
 
-    r2 = Convolution2D(192, 1, 1, activation='relu', bias=False)(input)
-    r2 = Convolution2D(192, 3, 3, activation='relu', subsample=(2, 2), bias=False)(r2)
+    r2 = conv_block(input, 256, 1, 1)
+    r2 = conv_block(r2, 256, 1, 7)
+    r2 = conv_block(r2, 320, 7, 1)
+    r2 = conv_block(r2, 320, 3, 3, subsample=(2, 2), border_mode='valid')
 
-    r3 = Convolution2D(256, 1, 1, activation='relu', border_mode='same', bias=False)(input)
-    r3 = Convolution2D(256, 1, 7, activation='relu', border_mode='same', bias=False)(r3)
-    r3 = Convolution2D(320, 7, 1, activation='relu', border_mode='same', bias=False)(r3)
-    r3 = Convolution2D(320, 3, 3, activation='relu', border_mode='valid', subsample=(2,2), bias=False)(r3)
+    r3 = MaxPooling2D((3, 3), strides=(2, 2), border_mode='valid')(input)
 
     m = merge([r1, r2, r3], mode='concat', concat_axis=channel_axis)
-    m = BatchNormalization(axis=channel_axis)(m)
-    m = Activation('relu')(m)
     return m
 
 
-def create_inception_v4(nb_classes=1001):
+def create_inception_v4(nb_classes=1001, load_weights=True):
     '''
     Creates a inception v4 network
 
@@ -195,7 +203,7 @@ def create_inception_v4(nb_classes=1001):
         x = inception_C(x)
 
     # Average Pooling
-    x = AveragePooling2D((8,8))(x)
+    x = AveragePooling2D((8, 8))(x)
 
     # Dropout
     x = Dropout(0.8)(x)
@@ -205,13 +213,25 @@ def create_inception_v4(nb_classes=1001):
     out = Dense(output_dim=nb_classes, activation='softmax')(x)
 
     model = Model(init, out, name='Inception-v4')
+
+    model.summary()
+
+    if load_weights:
+        if K.backend() == "tensorflow":
+            if K.image_dim_ordering() == "tf":
+                model.load_weights(TF_BACKEND_TF_DIM_ORDERING, by_name=True)
+            else:
+                print("Weights for Tensorflow backend TH dim ordering not ported yet.")
+        else:
+            print("Weights for Theano backend TH and TF dim ordering not ported yet.")
+
     return model
 
 
 if __name__ == "__main__":
-    from keras.utils.visualize_util import plot
+    # from keras.utils.visualize_util import plot
 
-    inception_v4 = create_inception_v4()
+    inception_v4 = create_inception_v4(load_weights=True)
     #inception_v4.summary()
 
     #plot(inception_v4, to_file="Inception-v4.png", show_shapes=True)
